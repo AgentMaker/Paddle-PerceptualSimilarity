b2a55c60fefa95c32ed7bee31b4a7ce769a224b4
Hecong Wu <HighCWu@163.com>
2021-05-17 13:59:02 +0800

    fix `setup.py` install packages.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/AgentMaker/Paddle-PerceptualSimilarity",
-     packages=['paddle_lpips'],
+     packages=['paddle_lpips', 'paddle_lpips/models'],
      package_data={'paddle_lpips': ['weights/v0.0/*.pdparams','weights/v0.1/*.pdparams']},
      include_package_data=True,
      classifiers=[
